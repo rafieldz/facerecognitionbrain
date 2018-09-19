3e064a08d625ae21bad55a173b936dc5ffc0de23
Rafael <rflassumpcao@gmail.com>
2018-09-19 11:04:57 -0300

    building components finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+
+const particlesOptions = {
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 800
+            }
+          }
+        }
+      }
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Navigation />
+      <Particles className='particles'
+        params={particlesOptions}
+        />
       <Logo />
+      <Rank />
           <ImageLinkForm />
               {/*    <FaceRecognition />*/}
       </div>
